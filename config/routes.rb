eb41b2bfee32b05926e53db5af7a5e1366232e5e
knguyen111601 <knguyen111601@gmail.com>
2022-06-01 10:32:36 -0700

    created root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index' # makes home page the root
+  
 
   # Defines the root path route ("/")
   # root "articles#index"
